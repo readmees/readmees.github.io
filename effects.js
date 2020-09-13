eb6a231914f068ea6aeea655322aca4d39123c68
README.es <mmeester@hotmail.nl>
2020-09-13 19:17:24 +0200

    Effects are now added as well

---
+++ effects.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#linkedin").hover(function(){
+        $("#linkedin").html('<img class="social_media" src="https://img.icons8.com/clouds/200/000000/linkedin.png"/>');
+    });
+});
+
+$(document).ready(function(){
+    $("#facebook").hover(function(){
+        $("#facebook").html('<img class="social_media" src="https://img.icons8.com/clouds/200/000000/facebook-new.png"/>');
+    });
+});
+
