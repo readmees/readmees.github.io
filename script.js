e8e8ea3d559b00175865a7f916b121e8d184bacb
README.es <mmeester@hotmail.nl>
2020-09-15 20:46:13 +0200

    Added maps iframe

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
        // When the user scrolls down 20px from the top of the document, slide down the navbar
-       window.onscroll = function() {scrollFunction(); scrollbar();};
+       window.onscroll = function() {scrollFunction(); scrollbar(); contact();};
 
        function scrollFunction() {
          if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
            document.getElementById("top_menu").style.top = "0";
            document.getElementById("social_media").style.bottom = "0";
            document.getElementById("myBar").style.top = "0";
-         } else {
+         }
+         else {
            document.getElementById("top_menu").style.top = "-50px";
            document.getElementById("social_media").style.bottom = "-100px";
            document.getElementById("myBar").style.top = "-100px";
          }
        }
-             // When the user scrolls the page, use scrollbar
 
        function scrollbar() {
          var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
